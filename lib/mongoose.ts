4cb5df9a35292b4bd89f6672a8a7838210beb43a
Ryan Carpenter <scrian_12@yahoo.com>
2024-02-14 16:51:56 -0500

    added question model

---
+++ lib/mongoose.ts
@@ -0,0 +1,49 @@
+// import mongoose from "mongoose";
+
+// const isConnected: boolean = false;
+
+// export const connectToDatabase = async () => {
+//   mongoose.set("strictQuery", true);
+
+//   if (!process.env.MONGODB_URL) {
+//     return console.log("missing missing mongodb url");
+//   }
+
+//   if (isConnected) {
+//     return console.log("MongoDB is already connected");
+//   }
+
+//   try {
+//     await mongoose.connect(process.env.MONGODB_URL, {
+//       dbName: "devflow",
+//     });
+//   } catch (error) {
+//     console.log("MongoDB connection failed", error);
+//   }
+// };
+
+import mongoose from "mongoose";
+
+let isConnected: boolean = false;
+
+export const connectToDatabase = async () => {
+  mongoose.set("strictQuery", true);
+
+  if (!process.env.MONGODB_URL) {
+    return console.log("Missing MONGODB_URL");
+  }
+
+  if (isConnected) {
+    return;
+  }
+
+  try {
+    await mongoose.connect(process.env.MONGODB_URL, {
+      dbName: "devflow",
+    });
+    isConnected = true;
+    console.log("MongoDB is connected");
+  } catch (error) {
+    console.log("MongoDB connection failed", error);
+  }
+};
